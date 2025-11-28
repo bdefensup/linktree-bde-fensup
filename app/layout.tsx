c9bbdd1342cfea2b7ed37447e985ee5d4efc13ea
Yanis Harrat <yanis.amine.harrat@gmail.com>
2025-11-28 02:12:38 +0100

    feat: Integrate Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import { Toaster } from "@/components/ui/sonner";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,6 +38,7 @@ export default function RootLayout({
         >
           {children}
           <Toaster position="bottom-right" expand={false} richColors />
+          <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
